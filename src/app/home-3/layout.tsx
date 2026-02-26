8263fecc13b29b51ade611f78a16298bbab68430
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2026-02-26 05:23:43 +0000

    hj

--- src/app/home-3/layout.tsx
+++ src/app/home-3/layout.tsx
@@ -1,13 +1,12 @@
- 
 import FooterThree from "@/components/footerThree";
 import Header from "@/components/header";
 import { ReactNode } from "react";
 
 export default function Home3Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en"> 
+    <html lang="en">
       <body className="bg-1c1c1c">
-        <Header className="white-navbar" logo="/images/white-logo.svg" />
+        <Header />
         {children}
         <FooterThree />
       </body>
